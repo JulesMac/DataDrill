5e9dad7fa3f7ad022533494785279fe5688bf66a
JULES <julianmackintosh@gmail.com>
2025-06-14 19:02:07 +0100

    Simplify Polars features

---
+++ tests/test_bindings.py
@@ -0,0 +1,16 @@
+import polars as pl
+import pytest
+
+try:
+    from datadrill_rs import sample_dataframe_with_modified_py
+except ImportError:
+    sample_dataframe_with_modified_py = None
+
+
+@pytest.mark.skipif(
+    sample_dataframe_with_modified_py is None, reason="bindings not compiled"
+)
+def test_sample_dataframe_from_rust():
+    df = sample_dataframe_with_modified_py()
+    expected = pl.DataFrame({"numbers": [1, 2, 3], "modified_numbers": [10, 20, 30]})
+    assert df.frame_equal(expected)
